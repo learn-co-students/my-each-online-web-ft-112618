30ecc04df127a19b5043232a87ce842f9c2ef23f
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-28 18:39:42 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  if block_given?
+  x = 0  
+
+    while x < words.length do
+      yield(words[x])
+    x += 1
+    end
+  end
+  words
 end
